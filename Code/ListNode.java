9c54d7bdb067190dce666c15fce0f903be121ff7
Haiyang <haiyang22@tutanota.com>
2022-09-05 09:39:04 -0400

    Updated for better readibility

--- Code/ListNode.java
+++ Code/ListNode.java
@@ -2,6 +2,11 @@ public class ListNode {
     int val;
     ListNode next;
     ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode(int val) { 
+        this.val = val; 
+    }
+    ListNode(int val, ListNode next) { 
+        this.val = val; 
+        this.next = next; 
+    }
 }
