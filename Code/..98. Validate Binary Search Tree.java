c852c09a9045972b962e55c8c7aec8c55aa8934b
Haiyang <haiyang22@tutanota.com>
2022-09-08 23:33:41 -0400

    Added solutions

---
+++ Code/..98. Validate Binary Search Tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+    private boolean helper(TreeNode root, Integer low, Integer high) {
+        if (root == null) {
+            return true;
+        }
+        if (low != null && root.val <= low) {
+            return false;
+        }
+        if (high != null && root.val >= high) {
+            return false;
+        }
+        return helper(root.left, low, root.val) && helper(root.right, root.val, high);
+    }
+}
